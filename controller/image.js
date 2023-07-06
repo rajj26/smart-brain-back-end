2b7e49438f30291c790af9ca28815a60232253e3
rajj26 <86844378+rajj26@users.noreply.github.com>
2023-07-07 01:24:27 +0600

    back end first commit

---
+++ controller/image.js
@@ -0,0 +1,83 @@
+const clarifai = require('clarifai');
+
+
+const setupClarifai = (imageUrl) => {
+    const PAT = '3019e98844444670871e5fde9dd3f399';
+      // Specify the correct user_id/app_id pairings
+      // Since you're making inferences outside your app's scope
+    const USER_ID = 'clarifai';       
+    const APP_ID = 'main';
+      // Change these to whatever model and image URL you want to use
+      // const MODEL_ID = 'face-detection';
+      // const MODEL_VERSION_ID = 'aa7f35c01e0642fda5cf400f543e7c40';  
+      // const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';  
+    const IMAGE_URL = imageUrl;
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+}; 
+
+    return requestOptions
+
+
+}
+
+const handleApiCall = (req, res) => {
+    fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, 
+          setupClarifai(req.body.input))
+          .then(data => {
+            res.json(data);
+          })
+          .catch(err => res.status(400).json('Unable to work with API'))
+}
+
+
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	
+	// let found = false;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0].entries);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+	// database.users.forEach(user => {
+	// 	if (user.id === id) {
+	// 		found = true;
+	// 		user.entries++
+	// 		return res.json(user.entries);
+	// 	}
+	// })
+	// 	if (!found) {
+	// 	res.status(400).json("not found");
+	// }
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+// const bcrypt1 = require('bcrypt');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controller/register.js');
+const signin = require('./controller/signin');
+const profile = require('./controller/profile');
+const image = require('./controller/image.js');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+	  host : '127.0.0.1',
+	  port : 5432,
+	  user : 'postgres',
+	  password : '112233',
+	  database : 'smart-brain'
+	}
+  });
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get('/', (req, res) => {
+	res.send('success');
+})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)} )  //this is called dependency injection
+
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000');
+})
