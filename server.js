5961dda33267ba8af0a64bf904b840db49853b8d
rajj26 <86844378+rajj26@users.noreply.github.com>
2023-07-11 01:00:09 +0600

    changes in pckge json and server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
-// const bcrypt1 = require('bcrypt');
 const cors = require('cors');
 const knex = require('knex');
 
@@ -23,8 +22,9 @@ const db = knex({
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
 
 
 
@@ -33,17 +33,9 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
-
-
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)} )  //this is called dependency injection
-
-
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-
-
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-
-
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 
