b5e04e30549d751fa6780250afa5091279cab25a
rajj26 <86844378+rajj26@users.noreply.github.com>
2023-07-12 12:13:44 +0600

    changes in server

--- controller/image.js
+++ controller/image.js
@@ -49,7 +49,7 @@ const handleApiCall = (req, res) => {
     fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, 
           setupClarifai(req.body.input))
           .then(data => {
-            res.json(data);
+            res.text(data);
           })
           .catch(err => res.status(400).json('Unable to work with API'))
 }
